ef2bab9dc3a6c577833dbab7ff36adf363e08374
Coles <coles.81@asc.ohio-state.edu>
2017-11-01 10:18:39 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+'''
+@title centroiding
+@author: Rebecca Coles
+Updated on Nov 1, 2017
+Created on Nov 1, 2017
+
+centroiding
+This project is used to find the center of various circles present in an image.
+'''
+
+# Import #######################################################################################
+################################################################################################
+
+if __name__ == '__main__':
+    pass
