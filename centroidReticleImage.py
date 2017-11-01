946c2037bac7046a15c6296d9ebbb7421e465012
Coles <coles.81@asc.ohio-state.edu>
2017-11-01 11:03:48 -0400

    updating functions to load fits images

---
+++ centroidReticleImage.py
@@ -0,0 +1,23 @@
+'''
+@title centroidReticleImage
+@author: Rebecca Coles
+Updated on Nov 1, 2017
+Created on Nov 1, 2017
+
+centroidReticleImage
+This module holds a series of functions used to find the center of
+a reticle from an image.
+
+Modules:
+'''
+
+# Import #######################################################################################
+################################################################################################
+
+class centroidReticleImage(object):
+    
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
