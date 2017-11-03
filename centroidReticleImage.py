f187114d537f0b06c75f5da1269b16d637eb5557
Coles <coles.81@asc.ohio-state.edu>
2017-11-03 13:01:35 -0400

    adding centroiding

--- centroidReticleImage.py
+++ centroidReticleImage.py
@@ -1,7 +1,7 @@
 '''
 @title centroidReticleImage
 @author: Rebecca Coles
-Updated on Nov 2, 2017
+Updated on Nov 3, 2017
 Created on Nov 1, 2017
 
 centroidReticleImage
@@ -14,8 +14,9 @@ findCentroid
 '''
 
 # Import #######################################################################################
-from cv2 import HoughCircles, HOUGH_GRADIENT, circle, rectangle, imshow, waitKey, putText, FONT_HERSHEY_SIMPLEX
-from numpy import round, hstack, copy, uint8
+from cv2 import HoughCircles, HOUGH_GRADIENT, circle, rectangle, putText, FONT_HERSHEY_SIMPLEX, cvtColor, COLOR_GRAY2RGB
+from numpy import round, hstack, copy, uint8, array, uint16, repeat
+from scipy.misc import toimage
 ################################################################################################
 
 class centroidReticleImage(object):
@@ -25,7 +26,7 @@ class centroidReticleImage(object):
         Constructor
         '''
         
-    def findCentroid(self, image, minRadius):
+    def findCentroid(self, image, minRadius, maxRadius):
         '''
         Take a numpy array of an image and centroid the circles within
         '''
@@ -33,20 +34,24 @@ class centroidReticleImage(object):
         output = copy(image)
         print('Image dimensions: ', output.shape)
         #Find the circles (convert image from uint16 (FITS 16bit) to 8bit)
-        circles = HoughCircles(uint8(output), HOUGH_GRADIENT, 1.2, minRadius)
-        print('found circles')
-        #Ensure at least some circles were found
+        circles = HoughCircles(uint8(output), HOUGH_GRADIENT, 4, 100, minRadius, maxRadius)
+        #Ensure at least some circles were foundIMREAD_COLOR
         if circles is not None:
+            print('found circles')
             #Convert the (x, y) coordinates and radius of the circles to integers
             circles = round(circles[0, :]).astype("int")
+            #Convert image to RGB
+            output.resize((output.shape[0], output.shape[1], 1))
+            outputColor = repeat(output.astype(uint8), 3, 2)
             #Loop over the (x, y) coordinates and radius of the circles
             for (x, y, r) in circles:
                 #Draw the circle in the output image, then draw a rectangle
                 #Corresponding to the center of the circle
-                circle(output, (x, y), r, (0, 255, 0), 4)
-                rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
-                putText(output,"Centroid locations: " + '(' + str(x) + ',' + str(y) + ') r=' + str(r),
+                circle(outputColor, (x, y), r, (0, 255, 0), 4)
+                rectangle(outputColor, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+                putText(outputColor,"Centroid locations: " + '(' + str(x) + ',' + str(y) + ') r=' + str(r),
                          (0,0), FONT_HERSHEY_SIMPLEX, 4,(255,255,255),2)
-            # show the output image
-            imshow("output", hstack([image, output]))
-            waitKey(0)
+            #Save Image
+            toimage(outputColor, cmin=0.0).save('outfile.jpg')
+        else:
+            print('No circles found')
